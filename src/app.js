78706e194c6133aec41541bd1f9e2a022fe4f9b3
Danny Shi <dannyshi.sr@gmail.com>
2020-07-31 13:35:26 -0400

    fixed syntax

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') ? "tiny" : "common";
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === 'production')) {
+  if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
     console.error(error);
