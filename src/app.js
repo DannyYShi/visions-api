29ca7b638fccd839ae2f392bc76f3050d3e01749
Danny Shi <dannyshi.sr@gmail.com>
2020-11-17 09:34:51 -0500

    small updates

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const ShopRouter = require('./shop/shop-router')
+
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("You have reached the Visions API");
@@ -29,6 +31,6 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
-app.use(cors());
+
 
 module.exports = app;
