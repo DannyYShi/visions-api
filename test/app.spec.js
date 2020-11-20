9cbd95f1b17fb3a115db9dfdf818c9e29cb7b5ff
Danny Shi <dannyshi.sr@gmail.com>
2020-11-20 09:51:04 -0500

    finished integration testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,6 +3,7 @@ const { expect } = require('chai')
 const supertest = require('supertest')
 const knex = require('knex')
 const { DATABASE_URL } = require("../src/config");
+const ShopService = require('../src/shop/shop-service')
 
 before(() => {
   db = knex({
@@ -12,7 +13,7 @@ before(() => {
 })
 
 describe("App", () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200 containing welcome message', () => {
     return supertest(app).get("/").expect(200, "You have reached the Visions API");
   });
 });
@@ -22,15 +23,25 @@ describe('Shop', () => {
   it('GET /api/shop responds with all of the shop items', () => {
     return supertest(app).get('/api/shop').expect(200).expect('Content-Type', /json/)
   })
+  it('GET /api/shop/:item_id responds with the corresponding item object', () => {
+    return supertest(app).get('/api/shop/1').expect(200).expect('Content-Type', /json/)
+  })
 })
 
 
-// describe("Lists", () => {
-//   describe(`getAllLists()`, () => {
-//     it('GET /api/lists responds with all the lists', () => {
-//       return ListsService.getAllLists(db).then((lists) => {
-//         expect(lists).to.be.a("array")
-//       })
-//     })
-//   })
-// })
+describe("Routers", () => {
+  describe(`getAllItems()`, () => {
+    it('GET /api/shop responds with all the items', () => {
+      return ShopService.getAllItems(db).then((items) => {
+        expect(items).to.be.a("array")
+      })
+    })
+  })
+  describe(`getById()`, () => {
+    it('GET /api/shop/:item_id responds with an item object', () => {
+      return ShopService.getById(db, 1).then((item) => {
+        expect(item).to.be.a("object")
+      })
+    })
+  })
+})
