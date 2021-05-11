40e9a8f93ae7cad32ff8c9da11ccc4438c1fcb52
Danny Shi <dannyshi.sr@gmail.com>
2021-05-11 13:52:25 -0400

    added error message to error handler

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use("/api/orders", OrderRouter);
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
+    response = { error: { message: error.message, error } };
   } else {
     console.error(error);
     response = { message: error.message, error };
